8d16c3ab79ef51460bf84084638b47b63dd89a97
Helitha <xillaron@live.com>
2018-09-15 00:43:03 +1200

    Added basic webpage with working user inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,59 @@
 import * as React from 'react';
-import './App.css';
 
-import logo from './logo.svg';
+interface IState {
+    zipcode: any
+}
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component<{}, IState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            zipcode: ""
+        }
+    }
+
+    public test(): void {
+        alert('your zipcode is ' + this.state.zipcode)
+    }
+
+    public handleOnClick(event: any): void {
+        if(this.state.zipcode.length === 4) {
+            this.test()
+		} else {
+            alert("Please enter a valid zipcode")
+        }
+    }
+    
+    
+    public handleOnChange(event: any): void {
+        this.setState({ zipcode: event.target.value });
+    }
+
+    public render() {
+        return (
+            
+            <div>
+                <div className='header'>
+                    <h2>Weather Forcast</h2>
+                </div>
+
+                <div className="instructions">
+                    <p>Enter a NZ zipcode below to get the <br />
+                        current weather conditions for that area.</p>
+                </div>
+
+                <div className='inputZip'>
+                    <input 
+                        onChange={e => this.handleOnChange(e)} 
+                        name='zipcode' 
+                        type='text' 
+                        placeholder='Enter zipcode..' 
+                        />
+                    <button onClick={e => this.handleOnClick(e)}>ENTER</button>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import App from './App';
+
+export const AppRouter: React.StatelessComponent<{}> = () => {
+    return (
+
+        <BrowserRouter>
+            <div>
+                <main>
+                    <Route exact={true} path="/" component={App} />
+                </main>
+            </div>
+        </BrowserRouter>
+
+    );
+}
+
