2b5aed1ef2d26f18d4d6b961346397e581277e41
Helitha <xillaron@live.com>
2018-09-15 00:47:59 +1200

    Added API call to openweathermaps and fixed TSLINT

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import * as React from 'react';
 
+const Url1 = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const Url2 = ',nz&appid=7fcd27225ea4c1c2ad4a27517bf3ece0&units=metric';
+
 interface IState {
     zipcode: any
+    currentTemp: any
+    humidity: any
 }
 
 class App extends React.Component<{}, IState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            zipcode: ""
+            zipcode: "",
+            currentTemp: '',
+            humidity: ''
         }
     }
 
     public test(): void {
-        alert('your zipcode is ' + this.state.zipcode)
+        fetch(Url1 + this.state.zipcode + Url2)
+        .then(res => res.json())
+        .then(data => {
+            if(data.cod === '404') {
+                alert("Please enter a valid zipcode")
+            } else {
+                this.setState({
+                    currentTemp: Math.round(data.main.temp) + '°',
+                    humidity: data.main.humidity + '%'
+                })
+            }
+            
+        })
     }
 
     public handleOnClick(event: any): void {
@@ -51,6 +70,12 @@ class App extends React.Component<{}, IState> {
                         />
                     <button onClick={e => this.handleOnClick(e)}>ENTER</button>
                 </div>
+
+                <div className="results">
+                    {this.state.currentTemp}<br />
+                    {this.state.humidity}
+                </div>
+                
             </div>
         )
     }
