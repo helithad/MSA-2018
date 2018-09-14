01924397621a5baf1bcebed70c9551321342b9b1
Helitha <xillaron@live.com>
2018-09-15 01:17:51 +1200

    Added more variables to API call and updated tslin

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,14 @@ const Url2 = ',nz&appid=7fcd27225ea4c1c2ad4a27517bf3ece0&units=metric';
 
 interface IState {
     zipcode: any
-    currentTemp: any
+    temp: any
     humidity: any
+    windSpeed: any
+    WindDeg: any
+    currentWeather: any
+    currentDescript: any
+    cityName: any
+
 }
 
 class App extends React.Component<{}, IState> {
@@ -14,8 +20,15 @@ class App extends React.Component<{}, IState> {
         super(props);
         this.state = {
             zipcode: "",
-            currentTemp: '',
-            humidity: ''
+            temp: '',
+            humidity: '',
+            windSpeed: '',
+            WindDeg: '',
+            currentWeather: '',
+            currentDescript: '',
+            cityName: ''
+
+
         }
     }
 
@@ -25,13 +38,23 @@ class App extends React.Component<{}, IState> {
         .then(data => {
             if(data.cod === '404') {
                 alert("Please enter a valid zipcode")
+            } else if(data.cod === '500') {
+                alert("This location is on cooldown, please try another one")
             } else {
                 this.setState({
-                    currentTemp: Math.round(data.main.temp) + '°',
-                    humidity: data.main.humidity + '%'
+                    temp: Math.round(data.main.temp) + '°',
+                    humidity: data.main.humidity + '%',
+                    windSpeed: data.wind.speed + 'km/h',
+                    WindDeg: data.wind.deg,
+                    currentWeather: data.weather[0].main,
+                    currentDescript: data.weather[0].description,
+                    cityName: data.name
                 })
             }
             
+        })
+        .catch(err => {
+            console.log(err);
         })
     }
 
@@ -72,10 +95,15 @@ class App extends React.Component<{}, IState> {
                 </div>
 
                 <div className="results">
-                    {this.state.currentTemp}<br />
-                    {this.state.humidity}
+                    {this.state.temp}<br />
+                    {this.state.humidity}<br />
+                    {this.state.windSpeed}<br />
+                    {this.state.WindDeg}<br />
+                    {this.state.currentWeather}<br />
+                    {this.state.currentDescript}<br />
+                    {this.state.cityName}<br />
                 </div>
-                
+
             </div>
         )
     }
